2fc9f538314e2ba85cc4ffe9a813e0f4da3dee2c
joaohamasaki <hamasaki.joao@gmail.com>
2021-02-26 22:33:56 -0300

    Minha primeira model no laravel na aula de 26-02-2021

---
+++ app/Models/Funcionario.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Funcionario extends Model
+{
+    use HasFactory;
+    // criando as colunas do banco
+    protected $fillable = ['id', 'nome', 'endereco', 'email', 'telefone'];
+
+    /**
+     *  É possível alterar a chave primária assim:
+     *  protected $primaryKey = 'nome_da_pk';
+     * 
+     * Para não ser auto_increment é assim:
+     * public $increment = false;
+     * 
+     * Para definir o tipo:
+     * protected $keyType = 'string';
+     * 
+     * Para tirar os campos timestamps:
+     * public $timestamps = false;
+     * 
+     */
+
+    protected $table = 'Funcionario';
+}
